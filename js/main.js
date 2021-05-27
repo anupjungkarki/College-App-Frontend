86db353a4b4ecbdcd60def7482cd17a09658cb75
Anup Jung Karki <anupkarki2012@gmail.com>
2021-05-27 14:02:14 +0545

    change on form

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
+  // For Hiding the Nav Bar
         $('#bar').click(function() {
             $(this).toggleClass('open');
             $('#page-content-wrapper ,#sidebar-wrapper').toggleClass('toggled');
 
         });
+
+
+$('a').click(function(){
+    $('a').removeClass('active');
+    $(this).addClass('active');
+    
+})
